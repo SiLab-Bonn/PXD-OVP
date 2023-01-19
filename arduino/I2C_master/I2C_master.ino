0f438ff488297d5d9d3af9bc84541714b4830848
hansk <krueger@physik.uni-bonn.de>
2023-01-19 11:09:57 +0100

    fixed SerialRead

--- arduino/I2C_master/I2C_master.ino
+++ arduino/I2C_master/I2C_master.ino
@@ -6,14 +6,13 @@
 
 #include <Wire.h>
 #include <Streaming.h>
+#include <ctype.h>
 
 #define OVP_I2C_ADD 0x3c
 #define BYTES_TO_READ 8
-#define FW_VERSION 2.0
+#define FW_VERSION 2.1
 
-const char startWriteMarker = '!';
-const char startReadMarker  = '?';
-const char stopMarker = '\n';
+const int OVP_STATUS_PIN = 2;
 
 unsigned char confReg;
 unsigned char mask0;
@@ -27,6 +26,7 @@ unsigned char stat2;
 void setup() {
   Wire.begin();        // join i2c bus (address optional for master)
   Serial.begin(9600);  // start serial for output
+  pinMode(OVP_STATUS_PIN, INPUT);
 
   Serial << "OVP board interface, FW version: " << FW_VERSION << endl;
 }
@@ -73,26 +73,47 @@ void ReadOVP()
 void ReadSerial()
 {
   unsigned char byteCount = 0;
-  unsigned char charCount = 0;
+  char charBuf[2] = "";
+  unsigned char temp;
   char rc;
-  String str;
-  
-  while ((Serial.available() > 0) && (byteCount < 4))  
+  boolean endTransmission = false;
+   
+  do 
   {
-    str = Serial.readStringUntil(' ');
+    while (Serial.available() < 1)  // wait for new character
+      ;
+    rc = Serial.read();
+    Serial << rc; // echo received data  
+
+    if ((rc == '\r') || (rc == '\n'))  // LF or CR received which terminates the transfer
+      endTransmission = true;
+
+    if (isxdigit(rc)) // HEX character received
     {
-      rc = strtol(str.c_str(), 0, 16);
-      switch (byteCount)
-      {
-        case 0: confReg = rc; break; 
-        case 1: mask0   = rc; break;
-        case 2: mask1   = rc; break;
-        case 3: mask2   = rc; break;
-        default: break;
-      }
-      byteCount ++;
+      charBuf[0] = charBuf[1];  // keep the last two received digits
+      charBuf[1] = rc;
     }
-  }
+    else if ((rc == ' ') || endTransmission)  // field separator found or LF or CR received
+    {
+      if (charBuf[0] != '\0')  // something valid received?
+      {
+        temp = strtol(charBuf, 0, 16);
+        switch (byteCount)
+        {
+          case 0: confReg = temp; break; 
+          case 1: mask0   = temp; break;
+          case 2: mask1   = temp; break;
+          case 3: mask2   = temp; break;
+          default: break;
+        }
+      }
+      if (endTransmission)  // everything received, reset counter
+        byteCount = 0;
+      else
+        byteCount ++;  
+      charBuf[0] = '\0';  // reset buffer for next word
+    }
+  } while ((byteCount < 4) && !endTransmission);  // do this until all bytes are processed or end of transmission received
 }
 
 void WriteSerial()
@@ -106,7 +127,8 @@ void WriteSerial()
          << _WIDTHZ(_HEX(confReg), 2) << " "
          << _WIDTHZ(_HEX(mask0), 2)   << " "
          << _WIDTHZ(_HEX(mask1), 2)   << " "
-         << _WIDTHZ(_HEX(mask2), 2)   << endl;
+         << _WIDTHZ(_HEX(mask2), 2)   << " "
+         << digitalRead(OVP_STATUS_PIN) << endl;
 }
 
 
